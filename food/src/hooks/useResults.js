e580e77193de437ac4f8ba8563e511d9558190f3
AlainDlcTVOP <alain_dlc@outlook.com>
2021-05-25 21:57:23 +0200

    bug fix

--- food/src/hooks/useResults.js
+++ food/src/hooks/useResults.js
@@ -2,36 +2,31 @@ import { useEffect, useState } from 'react';
 import yelp from '../api/yelp';
 
 export default () => {
-    const [results, setResults] = useState([]);
-    const [errorMessage, setErrorMessage] = useState('');
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
-    const searchApi = async searchTerm => {
-        console.log('--------------');
-        console.log('--------------');
-        console.log('UseResultPage');
-        console.log('--------------');
-        console.log('--------------');
-        console.log(results);
-        try {
-            const response = await yelp.get('/search', {
-                params: {
-                    limit: 30,
-                    term: searchTerm,
-                    location: 'Gothenburg'
-                }
-            });
-            setResults(response.data.businesses);
-        } catch (err) {
-            setErrorMessage('Something went wrong');
+  const searchApi = async searchTerm => {
+    console.log('Hi there!');
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 30,
+          term: searchTerm,
+          location: 'Gothenburg'
         }
-    };
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
+  };
 
-    // Call searchApi when component
-    // is first rendered.  BAD CODE!
-    // searchApi('pasta');
-    useEffect(() => {
-        searchApi('burgers');
-    }, []);
+  // Call searchApi when component
+  // is first rendered.  BAD CODE!
+  // searchApi('pasta');
+  useEffect(() => {
+    searchApi('pasta');
+  }, []);
 
-    return [searchApi, results, errorMessage];
+  return [searchApi, results, errorMessage];
 };
--- food/src/screens/ResultShowScreen.js
+++ food/src/screens/ResultShowScreen.js
@@ -3,48 +3,40 @@ import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
 import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({ navigation }) => {
-    const [result, setResult] = useState(null);
-    const id = navigation.getParam('id');
+  const [result, setResult] = useState(null);
+  const id = navigation.getParam('id');
 
-    const getResult = async (id) => {
-        const respone = await yelp.get(`/${id}`);
-        setResult(respone.data);
-    };
+  const getResult = async id => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+  useEffect(() => {
+    getResult(id);
+  }, []);
 
-    useEffect(() => {
-        getResult(id);
-    }, []);
+  if (!result) {
+    return null;
+  }
 
-    if (!result) {
-        return null;
-    }
-    console.log('--------------');
-    console.log('--------------');
-    console.log("ResultsShowScreen");
-    console.log('--------------');
-    console.log('--------------');
-    console.log(id);
-    console.log(result);
-    return (
-        <View>
-            <Text>{result.name}</Text>
-            <FlatList
-                data={result.photos}
-                keyExtractor={(photo) => photo}
-                renderItem={({ item }) => {
-                    return <Image style={styles.image} source={{ uri: item }} />;
-                }}
-            />
-        </View>
-    );
+  return (
+    <View>
+      <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={photo => photo}
+        renderItem={({ item }) => {
+          return <Image style={styles.image} source={{ uri: item }} />;
+        }}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    image: {
-        height: 200,
-        width: 300
-
-    }
+  image: {
+    height: 200,
+    width: 300
+  }
 });
 
 export default ResultsShowScreen;
